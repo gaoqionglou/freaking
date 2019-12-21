ecb9317b67dc277344c132ad78dbcd4dc43e77a0
gaoqionglou <438279375@qq.com>
2019-12-21 22:57:07 +0800

    download method

--- freak-core/src/main/java/com/kotlin/freak_core/net/RestClient.kt
+++ freak-core/src/main/java/com/kotlin/freak_core/net/RestClient.kt
@@ -2,11 +2,14 @@ package com.kotlin.freak_core.net
 
 import android.content.Context
 import com.kotlin.freak_core.net.callback.*
+import com.kotlin.freak_core.net.download.DownloadHandler
 import com.kotlin.freak_core.ui.FreakLoader
 import com.kotlin.freak_core.ui.LoaderStyle
-
+import okhttp3.MediaType
+import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
+import java.io.File
 import java.util.*
 
 class RestClient(
@@ -17,17 +20,23 @@ class RestClient(
     private val REQUEST: IRequest?,
     private val FAIL: IFail?,
     private val BODY: RequestBody?,
+    private val FILE: File?,
     private val LOADER_STYLE: LoaderStyle?,
-    private val CONTEXT: Context?
+    private val CONTEXT: Context?,
+
+    private val DOWNLOAD_DIR: String?,
+    private val EXTENSION: String?,
+    private val NAME: String?
+
 ) {
 
     private var PARAMS: WeakHashMap<String, Any> = RestCreator.ParamsHolder.PARAMS
 
     init {
 
-            params.forEach {
-                PARAMS[it.key] = it.value
-            }
+        params.forEach {
+            PARAMS[it.key] = it.value
+        }
 
     }
 
@@ -51,8 +60,19 @@ class RestClient(
         when (method.name) {
             HttpMethod.GET.name -> call = service.get(URL, PARAMS)
             HttpMethod.POST.name -> call = service.post(URL, PARAMS)
+            HttpMethod.POST_RAW.name -> call = service.postRaw(URL, BODY)
             HttpMethod.PUT.name -> call = service.put(URL, PARAMS)
+            HttpMethod.PUT_RAW.name -> call = service.putRaw(URL, BODY)
             HttpMethod.DELETE.name -> call = service.delete(URL, PARAMS)
+            HttpMethod.UPLOAD.name -> {
+                FILE?.let {
+                    val requestBody =
+                        RequestBody.create(MediaType.parse(MultipartBody.FORM.toString()), FILE)
+                    val body: MultipartBody.Part =
+                        MultipartBody.Part.createFormData("file", FILE.name, requestBody)
+                    call = service.upload(URL, body)
+                }
+            }
         }
 
         call?.enqueue(getRequestCallback())
@@ -68,15 +88,47 @@ class RestClient(
     }
 
     fun post() {
-        request(HttpMethod.POST)
+        if (BODY == null) {
+            request(HttpMethod.POST)
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw RuntimeException("params must be null when using postRaw body")
+            }
+            request(HttpMethod.POST_RAW)
+        }
     }
 
     fun put() {
-        request(HttpMethod.PUT)
+        if (BODY == null) {
+            request(HttpMethod.PUT)
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw RuntimeException("params must be null when using postRaw body")
+            }
+            request(HttpMethod.PUT_RAW)
+        }
     }
 
     fun delete() {
         request(HttpMethod.DELETE)
+    }
+
+    fun upload() {
+        request(HttpMethod.UPLOAD)
+    }
+
+    fun download() {
+        DownloadHandler(
+            URL,
+            PARAMS,
+            SUCCESS,
+            ERROR,
+            FAIL,
+            REQUEST,
+            DOWNLOAD_DIR,
+            EXTENSION,
+            NAME
+        ).handleDownload()
     }
 
 }
